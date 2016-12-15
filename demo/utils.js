6a2dfee31997eabe5ddf3d58885c3cec5b1a9fd0
lily1115 <1192157164@qq.com>
2016-12-15 21:38:25 +0800

    🔥

---
+++ demo/utils.js
@@ -0,0 +1,9 @@
+// 获取query
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
